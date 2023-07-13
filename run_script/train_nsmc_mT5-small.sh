ad4c9e6eef7af53f026d2e46e8c0b52bcfbcc41e
dalgarak <20063100+dalgarak@users.noreply.github.com>
2023-07-13 22:47:35 +0900

    fix mt5-small nsmc train example script

--- run_script/train_nsmc_mT5-small.sh
+++ run_script/train_nsmc_mT5-small.sh
@@ -1,1 +1,7 @@
-python train.py -save_path ./nsmc_test_mt5-small-baseline -init_model "google/mt5-small" -max_epoch 4 -learning_rate 1e-4 -gpus 4 -strategy ddp -float_precision 16 -grad_acc 4 -batch_size 16
+# nsmc-naive task. 별도의 프롬프트가 없음.
+python train.py -task nsmc-naive \
+	-save_path ./DELETEME-nsmc_test_mt5-small-baseline \
+	-init_model "google/mt5-small" \
+	-max_epoch 3 -learning_rate 1e-4 -gpus 2 \
+       	-strategy ddp -float_precision 32 \
+       	-grad_acc 4 -batch_size 4
